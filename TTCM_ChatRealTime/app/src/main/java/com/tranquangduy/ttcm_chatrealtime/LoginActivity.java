0b3e323b53dc590e644f541be5714477f8b61cd5
Your Name <duy05022000@gmail.com>
2021-05-16 21:30:47 +0700

    push

---
+++ TTCM_ChatRealTime/app/src/main/java/com/tranquangduy/ttcm_chatrealtime/LoginActivity.java
@@ -0,0 +1,33 @@
+package com.tranquangduy.ttcm_chatrealtime;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+    Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+        btnLogin = findViewById(R.id.btn_login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+
+
+}
---
+++ TTCM_ChatRealTime/app/src/main/java/com/tranquangduy/ttcm_chatrealtime/MainActivity.java
@@ -0,0 +1,42 @@
+package com.tranquangduy.ttcm_chatrealtime;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.nio.file.Files;
+
+public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomView);
+
+
+
+    }
+
+    private  BottomNavigationView.OnNavigationItemReselectedListener onNavigationItemReselectedListener
+            = new BottomNavigationView.OnNavigationItemReselectedListener() {
+        @Override
+        public void onNavigationItemReselected(@NonNull MenuItem item) {
+
+        }
+    };
+
+
+
+
+}
