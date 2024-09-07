3b56372ce41ec47ab3431c90e140b0d3e47b03e4
DuyTQ <duytq@seatech.com.vn>
2024-09-08 00:05:21 +0700

    update

--- TTCM_ChatRealTime/app/src/main/java/com/tranquangduy/control/APIService.java
+++ TTCM_ChatRealTime/app/src/main/java/com/tranquangduy/control/APIService.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public interface APIService {
     String NOTIFICATIOIN_URL = "https://fcm.googleapis.com/fcm/send";
-    String SEVER_KEY = "AAAA3I4loaY:APA91bGgiMwJmcef_opaAz8rfsF4Ykm1m999FUp0HgrwYVJvFx8EFf73L9cl2pCy_QTW0KgWQM0igD-RCbKP2j7UJoNsj2bxHkiWYbZ7NJBnbSkRG1iqPuTd7qnJB3Mf6RB3JA7E5z5f";
+    String SEVER_KEY = "AAAA3I4loaY:12312313123Jmcef_opaAz8rfsF4Ykm1m999FUp0HgrwYVJvFx8EFf73L9cl2pCy_QTW0KgWQM0igD-RCbKP2j7UJoNsj2bxHkiWYbZ7NJBnbSkRG1iqPuTd7qnJB3Mf6RB3JA7E5z5f";
     String CHANNEL_ID = "1000";
 
     void UPDATE_USERID(String id);
